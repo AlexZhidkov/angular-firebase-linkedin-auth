b83a6a08a28e34ae54d480afc154eab881372b56
Alexander Zhidkov <azhidkov@gmail.com>
2018-09-20 20:21:26 +0800

    deploy to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCPuFbhWHSAHl13msMDY61ec4KKjTNiYlU',
+    authDomain: 'chris-students-demo.firebaseapp.com',
+    databaseURL: 'https://chris-students-demo.firebaseio.com',
+    projectId: 'chris-students-demo',
+    storageBucket: 'chris-students-demo.appspot.com',
+    messagingSenderId: '613535702828'
+  }
 };
