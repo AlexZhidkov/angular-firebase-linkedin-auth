66139dc60b35a2eaf835e0e1e1f66003dc6351ef
Alexander Zhidkov <azhidkov@gmail.com>
2018-09-20 18:42:14 +0800

    add angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCPuFbhWHSAHl13msMDY61ec4KKjTNiYlU',
+    authDomain: 'chris-students-demo.firebaseapp.com',
+    databaseURL: 'https://chris-students-demo.firebaseio.com',
+    projectId: 'chris-students-demo',
+    storageBucket: 'chris-students-demo.appspot.com',
+    messagingSenderId: '613535702828'
+  }
 };
 
 /*
