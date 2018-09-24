fefb5d7959d8a88bc95fb2548c85e395418984ee
srwebdev5515 <nessaj55@yandex.com>
2018-09-24 04:32:22 +0200

    implement linkedin login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+
+declare var IN: any;
 
 @Component({
   selector: 'app-root',
@@ -11,12 +14,24 @@ export class AppComponent {
   items: Observable<any[]>;
   title = 'ChrisDemo';
 
-  constructor(db: AngularFirestore) {
+  constructor( db: AngularFirestore ) {
     this.items = db.collection('students').valueChanges();
   }
 
   loginWithLinkedIn() {
     // ToDo: log in with LinkedIn here and get the signed-In user information.
-    window.location.href = 'https://us-central1-internship-poc.cloudfunctions.net/redirect';
+    // window.location.href = 'https://us-central1-internship-poc.cloudfunctions.net/redirect';
+    IN.User.authorize(() => this.onLoggedIN());
+  }
+
+  async onLoggedIN() {
+    IN.API.Profile('me')
+      .fields('firstName', 'lastName', 'location', 'positions')
+      .result((profile) => {
+        this.items = of(profile.values);
+      })
+      .error((err) => {
+        console.log(err);
+      });
   }
 }
